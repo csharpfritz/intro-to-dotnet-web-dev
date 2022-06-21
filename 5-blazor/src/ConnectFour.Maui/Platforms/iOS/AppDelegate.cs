dff0e48d09325b42933ce02900523bc7628ca2ac
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-06-21 10:46:14 -0400

    Added a MAUI version

---
+++ 5-blazor/src/ConnectFour.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ConnectFour.Maui
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
